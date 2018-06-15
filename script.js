beb41895c3c1ed6e31d406215758aa9e2a0e7400
thiagometal <thiagometal.fne@gmail.com>
2018-06-15 03:05:38 -0400

    add listagem e style de produtos recomendados

--- script.js
+++ script.js
@@ -30,7 +30,7 @@ function searchItems() {
     const businessId = document.getElementById("businessIdInput").value;
     const url = 'http://localhost:3000/products/' + businessId; 
     
-    const ul = document.getElementById('dataProd');
+    const recommendList = document.getElementById('recommendList');
     const prodImageAux = document.getElementById('prodImage');
     const prodNameAux = document.getElementById('prodName');
     const prodPriceAux = document.getElementById('prodPrice');
@@ -41,9 +41,68 @@ function searchItems() {
     .then(function(result) {
         prodRef = result.data.references.item[0];
         prodImageAux.src = prodRef.imageName;
-        prodNameAux.innerHTML = `${prodRef.name}`
-        prodPriceAux.innerHTML = `${prodRef.price}`
+        prodNameAux.innerHTML = `${prodRef.name}`;
+        prodPriceAux.innerHTML = `${prodRef.price}`;
         prodPayCondAux.innerHTML = `${prodRef.productInfo.paymentConditions}`;
+
+        itensRecommend = result.data.recommendation;
+
+        return itensRecommend.map(function(item) {
+            var i = 1;
+            
+            let div = createNode('div'),
+                itemImage = createNode('img'),
+                itemName = createNode('span'),
+                itemLabel01 = createNode('label'),
+                itemPrice = createNode('span'),
+                itemPayCond = createNode('span'),
+                itemLabel02 = createNode('label');
+                br1 = createNode('br');
+                br2 = createNode('br');
+                br3 = createNode('br');
+                br4 = createNode('br');
+
+            itemImage.className = "prodImage";
+            itemImage.id = "prodImage" + i;
+            itemImage.src = item.imageName;
+
+            itemName.className = "prodName";
+            itemName.id = "prodName" + i;
+            itemName.innerHTML = `${item.name}`;
+
+            itemLabel01.className = "prodLabel";
+            itemLabel01.innerHTML = "Por:";
+
+            itemPrice.className = "prodPrice";
+            itemPrice.id = "prodPrice" + i;
+            itemPrice.innerHTML = `${item.price}`;
+
+            itemPayCond.className = "prodPayCond";
+            itemPayCond.id = "prodPayCond" + i;
+            itemPayCond.innerHTML = `${item.productInfo.paymentConditions}`;
+
+            itemLabel02.className = "prodLabel";
+            itemLabel02.innerHTML = "sem juros";
+
+            div.className = "prodRecommend";
+
+            br1.id = "br1";
+            br2.id = "br2";
+            br3.id = "br3";
+            br4.id = "br4";
+
+            append(div, itemImage);
+            append(div, br1);
+            append(div, itemName);
+            append(div, br2);
+            append(div, itemLabel01);
+            append(div, itemPrice);
+            append(div, br3);
+            append(div, itemPayCond);
+            append(div, br4);
+            append(div, itemLabel02);
+            append(recommendList, div);
+        })
     })
     .catch(error => console.log(error))
 }

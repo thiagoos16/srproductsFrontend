c23bdcb959990ae0a9b597e09e98aaad224768ad
thiagometal <thiagometal.fne@gmail.com>
2018-06-15 19:26:18 -0400

    aprimora estilo do carrossel

--- carrossel.js
+++ carrossel.js
@@ -10,9 +10,28 @@
         console.log(totalChildren);
         dir === "left" ? ++count : --count;
         listEl.style.left = count * 215 + 'px';
-        btnLeftEl.style.display = count < 0 ? "block" : "none";
+        console.log(count);
+
+        if (count < 0) {
+            btnLeftEl.style.display = "block";
+            btnLeftEl.style.fill = "rgb(7,76,140)";
+        } else {
+            btnLeftEl.style.fill = "rgb(128, 120, 120, 0.842)";
+            btnLeftEl.style.display = "none";
+        }
+
+        // Here, 3 is the number displayed at any given time
+        if (count > 3-totalChildren) {
+            btnRightEl.style.display = "block";
+            btnRightEl.style.fill = "rgb(7,76,140)";
+        } else {
+            btnRightEl.style.fill = "rgb(128, 120, 120, 0.842)";
+            btnRightEl.style.display = "none";
+        }
+
+        //btnLeftEl.style.display = count < 0 ? "block" : "none";
         // Here, 4 is the number displayed at any given time
-        btnRightEl.style.display = count > 3-totalChildren ? "block" : "none";
+        //btnRightEl.style.display = count > 3-totalChildren ? "block" : "none";
   }
 
   btnLeftEl.addEventListener("click", function(e) {
